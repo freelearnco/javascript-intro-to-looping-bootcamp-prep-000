7672989c8c2cf5c0f12e61ffafccd86b25165407
chandan chainani <chainanichan@gmail.com>
2017-09-04 08:47:21 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push(`"I am ${i} strange loop${i > 1 ? '' : 's'}."`);
+  }
+  return array
+}
+function whileLoop(n) {
+  while(n) {
+    console.log(--n)
+  }
+  return 'done'
+}
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while(array.length > 0);
+  return array
+}
